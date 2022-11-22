acaac8f7787d920306e52e56c8c0a088552afebf
Jia Xu <javyenjiaxu@global.tencent.com>
2022-11-21 17:43:09 -0800

    Add 101

--- Questions/101. Symmetric Tree/symmetricTree/Solution.java
+++ Questions/101. Symmetric Tree/symmetricTree/Solution.java
@@ -1,25 +1,23 @@
-package maxDepthOfBinaryTree;
+package symmetricTree;
 
 import java.util.Arrays;
 
 class Solution {
-    public static int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
 
-        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    public static boolean isSymmetric(TreeNode root) {
+        return false;
     }
 
     public static void main(String[] args) {
-        test(3, new Integer[]{3, 9, 20, null, null, 15, 7});
-        test(2, new Integer[]{1, 2});
-        test(0, new Integer[]{null});
-        test(4, new Integer[]{1, 2, 3, 4, 5, 6, 6, 6, 6, 6});
+        test(false, new Integer[]{3, 9, 20, null, null, 15, 7});
+        test(false, new Integer[]{1, 2});
+        test(true, new Integer[]{null});
+        test(true, new Integer[]{1, 2, 2, 3, 3, 3, 3, 4, null, null, null, null, null, null, 4});
+        test(false, new Integer[]{1, 2, 2, 0, 3, 3, 3, 4, null, null, null, null, null, null, 4});
     }
 
-    private static void test(int expectedResult, Integer[] testData) {
-        int calculatedResult = maxDepth(readNode(0, testData));
+    private static void test(boolean expectedResult, Integer[] testData) {
+        boolean calculatedResult = isSymmetric(readNode(0, testData));
         boolean passed = calculatedResult == expectedResult;
         System.out.println((passed ? "Passed" : "Failed") + " - " + Arrays.toString(testData)
                 + (passed ? "" : " Calculated result: " + calculatedResult + "; expected result: " + expectedResult));
